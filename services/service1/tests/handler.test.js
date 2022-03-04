805f0c3fa623801aa4aaa583961a5fb271f19f59
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2022-03-04 18:06:31 +1100

    Changed to supplier service

--- services/service1/tests/handler.test.js
+++
@@ -1,8 +0,0 @@
-import * as handler from '../handler';
-
-test('main', async () => {
-  const response = await handler.main('event', 'context');
-
-  expect(response.statusCode).toEqual(200);
-  expect(typeof response.body).toBe("string");
-});
--- services/supplier-service/handler.ts
+++ services/supplier-service/handler.ts
@@ -1,7 +1,7 @@
 import sample from "sample";
 import lib from "../../libs";
 
-export async function main(event, context) {
+export async function main(event: any, context: any) {
   return {
     statusCode: 200,
     body: `Hello World! Via ${sample()} and ${lib()}.`,
