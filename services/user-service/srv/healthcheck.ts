88649b17706cf974dcd9acb585e39a5d244fd203
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2022-03-04 19:59:59 +1100

    Added user service boilerplate

--- services/user-service/srv/healthcheck.ts
+++ services/user-service/srv/healthcheck.ts
@@ -3,6 +3,6 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 export const main: APIGatewayProxyHandler = async () => {
   return {
     statusCode: 200,
-    body: `[OK] Healthcheck Supplier Service.`,
+    body: `[OK] Healthcheck User Service.`,
   };
 };
